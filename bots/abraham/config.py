dc217dd2b7308bab225d589a90622c2030fd7152
Gene Kogan <kogan.gene@gmail.com>
2022-08-10 07:39:11 -0700

    fix typo

--- bots/abraham/config.py
+++ bots/abraham/config.py
@@ -11,7 +11,7 @@ stage = os.environ.get("STAGE", "dev")
 config_dict = {
     "dev": {
         "guilds": [
-            channels.ABRAHAM_BOTDEV
+            channels.ABRAHAM_BOTDEV_GUILD_ID
         ],
         "allowed_channels": [
             channels.ABRAHAM_BOTDEV_ABRAHAM
