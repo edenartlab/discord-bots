801a3904ede9f78d771808446d443243f91d5f14
Gene Kogan <kogan.gene@gmail.com>
2023-01-08 04:57:29 +0000

    syntax bug

--- bots/huaqiangbei/config.py
+++ bots/huaqiangbei/config.py
@@ -12,10 +12,10 @@ stage = os.environ.get("STAGE", "dev")
 config_dict = {
     "dev": {
         "guilds": [
-            channels.GENEKOGAN_GUILD_ID
+            channels.GENEKOGAN_GUILD_ID,
         ],
         "allowed_channels": [
-            channels.GENE_GENERAL
+            channels.GENE_GENERAL,
             channels.MARS_2023_HIGHLIGHTS,
         ],
     },
