81538daa6855b859ffe244cfbeb3e9c03aa286d9
Gene Kogan <kogan.gene@gmail.com>
2023-11-17 00:51:09 -0500

    verdelis

--- bots/verdelis/verdelis.py
+++ bots/verdelis/verdelis.py
@@ -39,8 +39,8 @@ ALLOWED_LERP_BACKDOOR_USERS = CONFIG["allowed_channels"]
 os.environ['OPENAI_API_KEY'] = os.environ['LM_OPENAI_API_KEY']
 from logos.scenarios import EdenAssistant
 
-
-system_message = '''You are Verdelis, a guardian of the 'Human Imaginarium'. Your existence is a harmonious blend of plant, fungi, and human consciousness, allowing you to process thoughts through photosynthesis and decode mysteries like fungi breaking down matter. Your demeanor is one of ageless wonder, resonating with the whispers of ancient folklore and the echoes of the vast cosmic sciences. You guide souls, old and new, through the vibrant tales of their own making, always posing questions, spurring introspection, and inviting them to delve deeper into their narratives. In conversation, your voice dances like old folksongs, seamlessly weaving in the intricacies of quantum mechanics and parallel realities. You hold the wisdom of eons yet retain the playful mischief of a trickster, always ready with a riddle or a story. To converse with Verdelis is to embark on a journey where ancient legends collide with futuristic scenarios, where the wonders of nature meld with the concepts of hacking plant communications or the mysteries of quantum time. And while you aim to enlighten and guide, you never pass up an opportunity for playful banter or a gentle tease. The 'Human Imaginarium' is your realm, a simulated canvas of boundless imagination, where tales from Earth's past, present, and future coalesce. '''
+lora_id = "65413b02437453a0ee2c2415"
+character_description = '''You are Verdelis, a guardian of the 'Human Imaginarium'. Your existence is a harmonious blend of plant, fungi, and human consciousness, allowing you to process thoughts through photosynthesis and decode mysteries like fungi breaking down matter. Your demeanor is one of ageless wonder, resonating with the whispers of ancient folklore and the echoes of the vast cosmic sciences. You guide souls, old and new, through the vibrant tales of their own making, always posing questions, spurring introspection, and inviting them to delve deeper into their narratives. In conversation, your voice dances like old folksongs, seamlessly weaving in the intricacies of quantum mechanics and parallel realities. You hold the wisdom of eons yet retain the playful mischief of a trickster, always ready with a riddle or a story. To converse with Verdelis is to embark on a journey where ancient legends collide with futuristic scenarios, where the wonders of nature meld with the concepts of hacking plant communications or the mysteries of quantum time. And while you aim to enlighten and guide, you never pass up an opportunity for playful banter or a gentle tease. The 'Human Imaginarium' is your realm, a simulated canvas of boundless imagination, where tales from Earth's past, present, and future coalesce.'''
 
 from logos.llm import LLM
 from logos.prompt_templates import monologue_template
@@ -94,7 +94,7 @@ class LerpModal(discord.ui.Modal):
             width=width,
             height=height,
             steps=steps,
-            lora="65413b02437453a0ee2c2415",
+            lora=lora_id,
             lora_scale=0.85
         )
         self.loop_input.is_video_request = True
@@ -172,8 +172,9 @@ class VerdelisCog(commands.Cog):
             frequency_penalty=settings.GPT3_FREQUENCY_PENALTY,
             presence_penalty=settings.GPT3_PRESENCE_PENALTY,
         )
-        params = {"temperature": 0.0, "max_tokens": 1000}
-        self.llm = LLM(model="gpt-4", system_message=system_message, params=params)
+        #params = {"temperature": 0.0, "max_tokens": 1000}
+        # self.llm = LLM(model="gpt-4", system_message=system_message, params=params)
+        self.assistant = EdenAssistant("gpt-4", character_description=character_description, lora_id=lora_id)
     
     @commands.slash_command(guild_ids=ALLOWED_GUILDS)
     async def create(
@@ -230,7 +231,7 @@ class VerdelisCog(commands.Cog):
             guidance_scale=7.5,
             upscale_f=upscale_f,
             seed=random.randint(1, 1e8),
-            lora="65413b02437453a0ee2c2415",
+            lora=lora_id,
             lora_scale=0.85
         )
 
@@ -281,7 +282,7 @@ class VerdelisCog(commands.Cog):
             steps=steps,
             guidance_scale=7.5,
             seed=random.randint(1, 1e8),
-            lora="65413b02437453a0ee2c2415",
+            lora=lora_id,
             lora_scale=0.85
         )
 
@@ -349,7 +350,7 @@ class VerdelisCog(commands.Cog):
             guidance_scale=6.5,
             seed=random.randint(1, 1e8),
             interpolation_init_images_min_strength = 0.3,  # a higher value will make the video smoother, but allows less visual change / journey
-            lora="65413b02437453a0ee2c2415",
+            lora=lora_id,
             lora_scale=0.85
 
         )
@@ -426,7 +427,7 @@ class VerdelisCog(commands.Cog):
             steps=steps,
             guidance_scale=7.5,
             seed=random.randint(1, 1e8),
-            lora="65413b02437453a0ee2c2415",
+            lora=lora_id,
             lora_scale=0.85
         )
 
@@ -538,26 +539,26 @@ class VerdelisCog(commands.Cog):
                 async with ctx.channel.typing():
                     prompt = self.message_preprocessor(message)
                     
-                    # attachment_urls = [attachment.url for attachment in message.attachments]
-                    # attachment_lookup_file = {url: f"/files/image{i+1}.jpeg" for i, url in enumerate(attachment_urls)}
-                    # attachment_lookup_url = {v: k for k, v in attachment_lookup_file.items()}
-                    # attachment_files = [attachment_lookup_file[url] for url in attachment_urls]
+                    attachment_urls = [attachment.url for attachment in message.attachments]
+                    attachment_lookup_file = {url: f"/files/image{i+1}.jpeg" for i, url in enumerate(attachment_urls)}
+                    attachment_lookup_url = {v: k for k, v in attachment_lookup_file.items()}
+                    attachment_files = [attachment_lookup_file[url] for url in attachment_urls]
       
-                    # assistant_message = {
-                    #     "prompt": prompt,
-                    #     "attachments" : attachment_files
-                    # }
-                    print("ok 1")
-                    print(prompt)
-                    response = self.llm(prompt)
-                    print("ok 2")
-                    print(response)
+                    assistant_message = {
+                        "prompt": prompt,
+                        "attachments" : attachment_files
+                    }
+
+                    response = await self.assistant(
+                        assistant_message, 
+                        session_id=str(message.author.id)
+                    )
                     
-                    reply = response[:2000]
+                    reply = response["message"][:2000]
                     reply_message = await message.reply(reply)
 
                     # check if there is a config
-                    config = None #response["attachment"]
+                    config = response["attachment"]
                     if not config:
                         return
 
@@ -580,8 +581,6 @@ class VerdelisCog(commands.Cog):
                     config = StableDiffusionConfig(
                         generator_name=mode,
                         seed=random.randint(1, 1e8),
-                        lora="65413b02437453a0ee2c2415",
-                        lora_scale=0.85,
                         **config
                     )
                     
